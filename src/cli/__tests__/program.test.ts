736ca1720fb1b5fde1ef317d12aa810030776d71
Julien Andreu <julienandreu@me.com>
2021-12-19 15:49:23 -0500

    feat(program): addDescription inject now the program description

--- src/cli/__tests__/program.test.ts
+++ src/cli/__tests__/program.test.ts
@@ -1,26 +1,45 @@
 import { Command } from 'commander';
-import { addVersion, createProgram } from '../program';
+import { addDescription, addVersion, createProgram } from '../program';
 
-describe('createProgram', () => {
-  it('should create a new empty program', () => {
-    const program = createProgram();
-    expect(program).toBeInstanceOf(Command);
+describe('program', () => {
+  describe('createProgram', () => {
+    it('should create a new empty program', () => {
+      const program = createProgram();
+      expect(program).toBeInstanceOf(Command);
+    });
   });
-});
 
-describe('addVersion', () => {
-  it('should set the version as option', () => {
-    const expectedVersion = '1.0.0-test';
-    const program = new Command();
-    const programVersionMock = jest.spyOn(program, 'version').mockImplementation();
-    addVersion(expectedVersion)(program);
-    expect(programVersionMock).toBeCalledWith(expectedVersion);
-    programVersionMock.mockRestore();
+  describe('addVersion', () => {
+    it('should set the version as option', () => {
+      const expectedVersion = '1.0.0-test';
+      const program = new Command();
+      const programVersionMock = jest.spyOn(program, 'version').mockImplementation();
+      addVersion(expectedVersion)(program);
+      expect(programVersionMock).toBeCalledWith(expectedVersion);
+      programVersionMock.mockRestore();
+    });
+    it('should return the program', () => {
+      const expectedVersion = '1.0.0-test';
+      const program = new Command();
+      const versionedProgram = addVersion(expectedVersion)(program);
+      expect(versionedProgram).toBeInstanceOf(Command);
+    });
   });
-  it('should return the program', () => {
-    const expectedVersion = '1.0.0-test';
-    const program = new Command();
-    const versionedProgram = addVersion(expectedVersion)(program);
-    expect(versionedProgram).toBeInstanceOf(Command);
+
+  describe('addDescription', () => {
+    it('should set the description', () => {
+      const expectedDescription = 'description';
+      const program = new Command();
+      const programDescriptionMock = jest.spyOn(program, 'description').mockImplementation();
+      addDescription(expectedDescription)(program);
+      expect(programDescriptionMock).toBeCalledWith(expectedDescription);
+      programDescriptionMock.mockRestore();
+    });
+    it('should return the program', () => {
+      const expectedDescription = 'description';
+      const program = new Command();
+      const versionedProgram = addDescription(expectedDescription)(program);
+      expect(versionedProgram).toBeInstanceOf(Command);
+    });
   });
 });
--- src/cli/program.ts
+++ src/cli/program.ts
@@ -6,3 +6,8 @@ export const addVersion =
   (version: string) =>
   (program: Command): Command =>
     program.version(version);
+
+export const addDescription =
+  (description: string) =>
+  (program: Command): Command =>
+    program.description(description);
